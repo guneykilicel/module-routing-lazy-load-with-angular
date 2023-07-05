86089b9a5350d706c6b488a2b904c2d0eb26fb82
guneykilicel <kilicelguney@gmail.com>
2023-07-05 14:51:53 +0300

    app.routing.module'e tasidik

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,12 @@ import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
+  {path:'placeholders',loadChildren: () => 
+  import('./placeholders/placeholders.module').then(m => m.PlaceholdersModule)
+  },
+  {path:'tables',loadChildren: () => 
+  import('./tables/tables.module').then(m => m.TablesModule)
+  },
   {path:'',component:HomeComponent},
   {path:'**',component:NotFoundComponent}
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PlaceholdersModule } from './placeholders/placeholders.module';
-import { TablesModule } from './tables/tables.module';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
@@ -16,8 +13,6 @@ import { NotFoundComponent } from './not-found/not-found.component';
   ],
   imports: [
     BrowserModule,
-    PlaceholdersModule,
-    TablesModule,
     AppRoutingModule
   ],
   providers: [],
